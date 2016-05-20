ee312d9b66e3840af5a699bd1514c8e0000ae5a4
Mateusz Bućko <mateuszbucko1@gmail.com>
2016-05-20 18:50:27 +0200

    first try to add assets

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    private float movex=0;
+    private float movey=0;
+    private float moveSpeed=5;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * moveSpeed, movey * moveSpeed);
+	}
+}
