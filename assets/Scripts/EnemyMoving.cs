ff8941d22f4a7f67f8fb0de99ea768ead192cb9d
Gabriel Samojło <gabrielsamojlo@gmail.com>
2016-05-20 19:28:52 +0200

    enemy movement

---
+++ assets/Scripts/EnemyMoving.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+	public float range;
+	public float speed;
+	private Rigidbody2D enemyBody;
+	private Vector2 startPosition;
+	private Vector2 currentPosition;
+	private bool isLeft;
+
+	void Start () {
+		enemyBody = gameObject.GetComponent<Rigidbody2D>();
+		startPosition = enemyBody.position;
+	}
+
+	void Update () {
+
+		currentPosition = enemyBody.position;
+
+		if (isLeft) {
+			if (currentPosition.x < startPosition.x + range)
+				enemyBody.AddForce (-Vector2.left * speed);
+			else isLeft = false;
+		} else {
+			if (currentPosition.x > startPosition.x - range)
+				enemyBody.AddForce(Vector2.left*speed);
+			else isLeft = true;
+			}
+
+
+			}
+}
