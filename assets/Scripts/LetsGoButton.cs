14e237b84000eb1fd1f696076e99d24e10171718
Gabriel Samojło <gabrielsamojlo@gmail.com>
2016-05-21 01:06:44 +0200

    menu and story menu

---
+++ assets/Scripts/LetsGoButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LetsGoButton : MonoBehaviour {
+
+	public void goToMainScene() {
+		SceneManager.LoadScene(2);
+	}
+}
