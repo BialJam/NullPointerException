b30d70ff942944725137da1dcdac61debf5addbd
Gabriel Samojło <gabrielsamojlo@gmail.com>
2016-05-20 20:31:18 +0200

    player chasing

---
+++ assets/PlayerChasing.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerChasing : MonoBehaviour {
+
+	public Rigidbody2D playerBody;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- assets/Scripts/EnemyMoving.cs
+++ assets/Scripts/EnemyMoving.cs
@@ -1,33 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Moving : MonoBehaviour {
+public class EnemyMoving : MonoBehaviour
+{
 	public float range;
 	public float speed;
+	public float seeRange;
+	private GameObject playerObject;
+	private Rigidbody2D playerBody;
 	private Rigidbody2D enemyBody;
 	private Vector2 startPosition;
 	private Vector2 currentPosition;
 	private bool isLeft;
+	private Vector2 playerPosition;
+	private bool isPlayerInRange;
 
-	void Start () {
-		enemyBody = gameObject.GetComponent<Rigidbody2D>();
+	void Start ()
+	{
+		enemyBody = gameObject.GetComponent<Rigidbody2D> ();
 		startPosition = enemyBody.position;
+		isPlayerInRange = false;
+		playerObject = GameObject.Find ("hero");
+		playerBody = playerObject.GetComponent<Rigidbody2D> ();
 	}
 
-	void Update () {
+	void Update ()
+	{
 
+		playerPosition = playerBody.position;
 		currentPosition = enemyBody.position;
 
-		if (isLeft) {
-			if (currentPosition.x < startPosition.x + range)
+		if (!isPlayerInRange) {
+			if (isLeft) {
+				if (currentPosition.x <= startPosition.x + range)
+					enemyBody.AddForce (-Vector2.left * speed);
+				else
+					isLeft = false;
+			} else {
+				if (currentPosition.x >= startPosition.x - range)
+					enemyBody.AddForce (Vector2.left * speed);
+				else
+					isLeft = true;
+			}
+		} 
+
+		if (playerPosition.x >= currentPosition.x - seeRange && playerPosition.x <= currentPosition.x + seeRange) {
+			if (playerPosition.x - currentPosition.x < 0) {
+				isPlayerInRange = true;
+				enemyBody.AddForce (Vector2.left * speed);
+			} else if (playerPosition.x - currentPosition.x > 0) {
 				enemyBody.AddForce (-Vector2.left * speed);
-			else isLeft = false;
-		} else {
-			if (currentPosition.x > startPosition.x - range)
-				enemyBody.AddForce(Vector2.left*speed);
-			else isLeft = true;
+				isPlayerInRange = true;
 			}
+		} else { 
+			isPlayerInRange = false;
+			// going to start position
+			if (currentPosition.x < startPosition.x)
+				enemyBody.AddForce (-Vector2.left * speed);
+			else
+				enemyBody.AddForce (Vector2.left * speed);
+		}	
 
-
-			}
+	}
 }
