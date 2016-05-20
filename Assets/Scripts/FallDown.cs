32bf241ea930fda4b23322ce7d70c8443344fb36
Gabriel Samojło <gabrielsamojlo@gmail.com>
2016-05-20 21:46:17 +0200

    falling down fix

--- Assets/Scripts/FallDown.cs
+++ Assets/Scripts/FallDown.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class FallDown : MonoBehaviour {
+	
+	private GameObject playerObject;
+	private Camera cameraObj;
 
 	// Use this for initialization
 	void Start () {
-	
+		playerObject = GameObject.Find ("hero");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float position = 0 - (Camera.current.orthographicSize);
+
+		if (playerObject.transform.position.y < position) {
+			PlayerStats.lifes--;
+			SceneManager.LoadScene(PlayerStats.ActualScene);
 	}
+}
+
 }
