aeaed559000e5d359a377b554758f7d6bd6951ff
Gabriel Samojło <gabrielsamojlo@gmail.com>
2016-05-21 14:54:41 +0200

    NPC dialog system

---
+++ assets/DialogController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogController : MonoBehaviour {
+
+	public string[] dialogs;
+	public string text;
+
+	private TextMesh npcTextMesh;
+	private TextMesh playerTextMesh;
+	private MeshRenderer npcRenderer;
+	private MeshRenderer playerRenderer;
+	private int index = 0;
+	int size;
+	void Start () {
+		npcTextMesh = this.GetComponentInChildren<TextMesh> ();
+		playerTextMesh = GameObject.Find("hero").GetComponentInChildren<TextMesh> ();
+		npcRenderer = this.GetComponentInChildren<MeshRenderer> ();
+		playerRenderer = GameObject.Find ("hero").GetComponentInChildren<MeshRenderer> ();
+		size = dialogs.Length;
+		StartCoroutine ("DialogSwitch");
+	}
+		
+	private bool checkIndex() {
+		if (index >= dialogs.Length) {
+			npcTextMesh.text = "";
+			playerTextMesh.text = "";
+			return false;
+		}
+		return true;
+	}
+		
+	IEnumerator DialogSwitch() {
+		while (size >= 0) {
+			if (!checkIndex()) {
+				break;
+			}
+			npcTextMesh.text = dialogs[index];
+			yield return new WaitForSeconds (2);
+			npcRenderer.enabled = false;
+			playerRenderer.enabled = true;
+			index++;
+			if (!checkIndex()) {
+				break;
+			}
+			playerTextMesh.text = dialogs[index];
+			yield return new WaitForSeconds (2);
+			npcRenderer.enabled = true;
+			playerRenderer.enabled = false;
+			index++;
+			size-=2;
+		}
+	}
+}
