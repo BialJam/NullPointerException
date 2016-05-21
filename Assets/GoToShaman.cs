698cdc9f1db240aaf3483ae7279d190e2830c7ee
Evanuke <ma.iw.f7@gmail.com>
2016-05-21 22:27:44 +0200

    Shaman things LOL

---
+++ Assets/GoToShaman.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToShaman : MonoBehaviour {
+
+	private GameObject player;
+	private Rigidbody2D playerBody;  
+
+	private GameObject Shaman;
+	private Rigidbody2D shamanBody;
+	private Animator anim;
+
+
+	void Start () {
+		player = GameObject.Find ("hero");
+		playerBody = player.GetComponent<Rigidbody2D> ();
+		Shaman = GameObject.Find ("Shaman");
+		anim = player.GetComponent<Animator>();
+
+	}
+	
+
+	void Update () {
+		float moveHorizontal = 1f;
+		if (player.transform.position.x < -2.5) 
+		{
+			playerBody.velocity = new Vector2 (moveHorizontal, playerBody.velocity.y);
+			anim.SetFloat ("speed", 1);
+
+		}
+		else
+		anim.SetFloat ("speed", 0);
+		}
+	}
+
