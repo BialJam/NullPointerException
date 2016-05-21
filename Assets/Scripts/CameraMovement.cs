77e4435d1f7509030f8e016e504418591e1dfca0
Gabriel Samojło <gabrielsamojlo@gmail.com>
2016-05-21 23:58:44 +0200

    camera without y cord following

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -18,9 +18,9 @@ public class CameraMovement : MonoBehaviour {
 	void FixedUpdate () {
 
 		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+		//float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
-		transform.position = new Vector3(posX, posY, transform.position.z);
+		transform.position = new Vector3(posX, transform.position.y, transform.position.z);
 	
 	}
 
