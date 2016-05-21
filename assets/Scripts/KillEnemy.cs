3bb6ddbb143908e8d74977de217b85e36c84d3c3
agata <agata>
2016-05-21 20:41:36 +0200

    poprawka killenemy

--- assets/Scripts/KillEnemy.cs
+++ assets/Scripts/KillEnemy.cs
@@ -53,27 +53,4 @@ public class KillEnemy : MonoBehaviour {
 			Destroy (gameObject);
 		}
 	}
-			
-    void OnTriggerEnter2D(Collider2D coll) {
-
-        if (coll.name == "killPoint") {
-            if (duplicated == false) {
-
-            dupEnemy1 = Instantiate(Resources.Load(enemy.name), new Vector3(enemy.transform.position.x - offset, enemy.transform.position.y, enemy.transform.position.z), Quaternion.identity) as GameObject;
-            dupEnemy1.GetComponent<KillEnemy>().duplicated = true;
-            dupEnemy2 = Instantiate(Resources.Load(enemy.name), new Vector3(enemy.transform.position.x + offset, enemy.transform.position.y, enemy.transform.position.z), Quaternion.identity) as GameObject;
-            dupEnemy2.GetComponent<KillEnemy>().duplicated = true;
-            Destroy(gameObject);
-            duplicated = true;
-        } else {
-            Destroy(gameObject);
-        }
-            
-    }	
-        if (coll.name == "hero")
-        {
-            PlayerStats.Lifes--;
-            SceneManager.LoadScene(PlayerStats.ActualScene);
-        }
-    }
 }
