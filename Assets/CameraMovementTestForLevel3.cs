bc6bb261f80757c6510e16439d946075abef0bea
Mateusz Bućko <mateuszbucko1@gmail.com>
2016-05-22 09:53:16 +0200

    shaman level update + prefabs

---
+++ Assets/CameraMovementTestForLevel3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private Vector2 velocity;
+
+	public float smoothTimeX;
+	public float smoothTimeY;
+
+	GameObject player;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+        int a;
+	}
+	
+
+	void FixedUpdate () {
+
+		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		//float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+		transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+	
+	}
+
+}
